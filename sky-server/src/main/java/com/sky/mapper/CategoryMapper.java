02d895aa3d3db3fbe054cbcf73ade6485fb6f6a9
HuangDDXD <149939518+HuangDDXD@users.noreply.github.com>
2024-06-10 18:13:47 +0800

    分页查询菜单分类代码业务

--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.entity.Category;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -7,8 +9,19 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface CategoryMapper {
 
+    /**
+     * 新增菜品分类
+     * @param category
+     */
     @Insert("insert into category (type, name, sort, status, create_time, update_time, create_user, update_user) " +
             "value " +
             "(#{type}, #{name}, #{sort}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void insert(Category category);
+
+    /**
+     * 分类分页查询
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    Page<Category> pageQuery(CategoryPageQueryDTO categoryPageQueryDTO);
 }
--- sky-server/src/main/java/com/sky/service/CategoryService.java
+++ sky-server/src/main/java/com/sky/service/CategoryService.java
@@ -2,6 +2,9 @@ package com.sky.service;
 
 
 import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.result.PageResult;
 
 public interface CategoryService {
     /**
@@ -9,4 +12,11 @@ public interface CategoryService {
      * @param categoryDTO
      */
     void add(CategoryDTO categoryDTO);
+
+    /**
+     * 分类分页查询
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    PageResult page(CategoryPageQueryDTO categoryPageQueryDTO);
 }
