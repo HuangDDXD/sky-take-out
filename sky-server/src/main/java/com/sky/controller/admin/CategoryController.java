5fc1ea718713de4ef7080ff0321ceb6de14342b9
HuangDDXD <149939518+HuangDDXD@users.noreply.github.com>
2024-06-10 13:52:03 +0800

    新增菜单分类代码业务

---
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -0,0 +1,44 @@
+package com.sky.controller.admin;
+
+
+import com.sky.dto.CategoryDTO;
+import com.sky.properties.JwtProperties;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * 分类管理
+ */
+@RestController
+@RequestMapping("/admin/category")
+@Slf4j
+@Api(tags = "分类相关接口")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    /**
+     * 新增菜品分类
+     * @param categoryDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增菜品分类")
+    public Result add(@RequestBody CategoryDTO categoryDTO) {
+        log.info("新增菜品分类");
+        categoryService.add(categoryDTO);
+        return Result.success();
+    }
+}
